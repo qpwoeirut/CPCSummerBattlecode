ec9f86382ed69fcb14cf3d354aa4e75ccca6b731
qpwoeirut <stanleyzhong8@gmail.com>
2023-06-24 02:30:00 -0700

    Add code to run against old c++ versions of bot

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
 g++-12 c++/v1/player.cpp -o c++/v1/player.out -O
 java -jar icypc.jar -player cpp c++/v1/player.out -player java -cp java Hunter
 rm c++/v1/player.out
+
+#g++-12 c++/v0/camper.cpp -o c++/v0/camper.out -O
+#g++-12 c++/v1/player.cpp -o c++/v1/player.out -O
+#java -jar icypc.jar -player cpp c++/v0/camper.out -player cpp c++/v1/player.out
+#rm c++/v0/camper.out
+#rm c++/v1/player.out
